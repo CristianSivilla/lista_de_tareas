f9a869943ae326875fc8e160596d395f7e3358c8
Cristian Sivillà <cristiansivillacalvo@gmail.com>
2025-10-27 15:09:37 +0100

    get de prueba

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+
+app.get("/", (requ, res) =>{
+    res.send("Server is ready");
+})
+
  app.listen(5000, () => {
     console.log('Servidor iniciado en http://localhost:5000 hello');
     
