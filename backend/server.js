389239d0d4597acc3854e0ad2e217bba58fe4343
Cristian Sivillà <cristiansivillacalvo@gmail.com>
2025-10-27 15:42:54 +0100

    configuracion db

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
-
 const app = express();
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 
-app.get("/", (requ, res) =>{
-    res.send("Server is ready");
+dotenv.config();
+
+app.get("/tareas", (requ, res) =>{
+
 })
 
+console.log(process.env.MONGO_URI);
+
  app.listen(5000, () => {
-    console.log('Servidor iniciado en http://localhost:5000 hello');
+    connectDB();
+    console.log('Servidor iniciado en http://localhost:5000');
     
  });
