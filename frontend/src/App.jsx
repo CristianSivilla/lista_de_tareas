a42f4f8a382a24056beccf4cf4c8fc05d9b6c6dd
Cristian Sivillà <cristiansivillacalvo@gmail.com>
2025-10-28 17:12:27 +0100

    empezando con el navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
 import { useState } from 'react'
 import { Button } from '@chakra-ui/react'
 import './App.css'
-import { Route } from 'react-router-dom'
 import CrearTarea from './paginas/CrearTarea';
 import Inicio from './paginas/Inicio';
-import Navbar from './components/ui/Navbar';
+import Nabvar from './components/ui/Navbar';
+import { Box } from '@chakra-ui/react'
+import { Routes, Route } from 'react-router-dom'
 
 
 
 function App() {
 
   return (
-    <Box minH={"100vh"}>
-      <Nabvar />
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/" element={<CrearTarea />} />
-        </Routes>
-    </Box>
+  <Box minH={"100vh"}>
+  <Nabvar />   
+  <Routes>
+    <Route path="/" element={<Inicio />} />
+    <Route path="/" element={<CrearTarea />} />
+  </Routes>
+</Box>
+
   )
 }
 
