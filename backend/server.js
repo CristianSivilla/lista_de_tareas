f278c600ce556dd79ea489738ad6421d60cea2e7
Cristian Sivillà <cristiansivillacalvo@gmail.com>
2025-10-28 12:10:42 +0100

    creacion del fichero de rutas

--- backend/server.js
+++ backend/server.js
@@ -1,72 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Tarea from './models/tarea.model.js';
+import tareaRutas from "./rutas/tarea.rutas.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); 
-
-app.get("/api/tareas", async(req, res) =>{
-  try{
-    const tareas = await Tarea.find({});
-    res.status(200).json({success:true, data:tareas})
-  }catch(error) {
-    console.log("Error al mostrar los productos", error.message);
-    res.status(500).json({success: false, message: "Server Error"});
-  }
-
-});
-app.post("/api/tareas", async (req, res) => {
-  const tarea = req.body;
-
-  if (!tarea.nombre || !tarea.descripcion || tarea.completado === undefined) {
-    return res.status(400).json({ success: false, message: "Por favor rellena todos los campos" });
-  }
-
-  const nuevaTarea = new Tarea(tarea);
-
-  try {
-    await nuevaTarea.save();
-    res.status(201).json({ success: true, data: nuevaTarea });
-  } catch (error) {
-    console.error("Error al crear la tarea:", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.put("/api/tareas/:id", async (req, res) => {
-  const {id} = req.params;
-  const tarea = req.body;
-  
-  try{
-   const tareActulizada = await Tarea.findByIdAndUpdate(id, tarea, {new: true});
-   res.status(200).json({succes:true, data: tareActulizada});
-  }catch(error){
-  res.status(500).json({succes: false, message: "Server Error"});
-
-  }
-
-})
-
-app.delete("/api/tareas/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log("id:", id);
-
-    try {
-        const deleted = await Tarea.findByIdAndDelete(id);
-
-        if (!deleted) {
-            return res.status(404).json({ success: false, message: "Tarea no encontrada" });
-        }
-
-        res.status(200).json({ success: true, message: "Tarea eliminada correctamente" });
-    } catch (error) {
-        console.error("No se ha podido eliminar la tarea:", error);
-        res.status(500).json({ success: false, message: "Error al eliminar la tarea" });
-    }
-});
+app.use("/api/tareas",tareaRutas);
 
 app.listen(5000, () => {
   connectDB();
