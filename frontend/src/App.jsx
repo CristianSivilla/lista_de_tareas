59efacc836ae56bc586cdfda435d8a235de3b62a
Cristian Sivillà <cristiansivillacalvo@gmail.com>
2025-10-28 16:33:32 +0100

    iniciamos chakra

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Button } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import App from "./App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
